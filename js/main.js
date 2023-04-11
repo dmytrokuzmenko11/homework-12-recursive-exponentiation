5ed368efde3fb2927620898c6c864eaa99b99cf5
Dmytro <106152611+dmytrokuzmenko11@users.noreply.github.com>
2023-04-11 22:42:22 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,21 @@
+
+// homework 12
+// recursive exponentiation
+
+
+let x = +prompt('Enter number', 5);
+let n = +prompt('Enter power of a number', 4)
+
+function pow(x, n) {
+    return (n == 1) ? x : (x * pow(x, n - 1));
+  }
+
+alert(`The number ${x} to the power of ${n} is equal ${pow(x, n)}`);
+document.write(`${x}<sup>${n}</sup> = ${pow(x, n)}`);
+
+
+// calculations of pow(5, 4) 
+// 5 * 1 = 5
+// 5 * 5 = 25
+// 5 * 25 = 125
+// 5 * 125 = 625
